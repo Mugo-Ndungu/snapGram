7c1d702a54a0c90fabe91b80afc10f18b82413f3
Mugo-Ndungu <twinnymugo@gmail.com>
2019-03-08 08:56:57 +0300

    Add virtual environment to project

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alex/Documents/Moringa School Projects/Python3/Django Projects/snapGram/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
