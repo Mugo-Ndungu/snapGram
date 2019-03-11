c26bab3b0fd07a31a96caeb44b1683bf78b6ff7f
Mugo-Ndungu <twinnymugo@gmail.com>
2019-03-11 18:31:27 +0300

    Add class post

--- snap/models.py
+++ snap/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from time import timezone
+from django.contrib.auth.models import User,AbstractBaseUser,BaseUserManager
+from django.core.mail import send_mail
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
-#cmmento?
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg',upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username}Profile'
+
+class Post(models.Model):
+    author = models.ForeignKey(User,related_name='post')
+    photo = models.ImageField(upload_to='Posts_photos/')
+    caption = models.CharField(max_length=60)
+
+class Like(models.Model):
+    post = models.ForeignKey(Post,related_name='liked_post')
+    user = models.ForeignKey(User, related_name='liker')
+
